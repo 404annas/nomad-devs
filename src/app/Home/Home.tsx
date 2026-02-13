bb9d8cd74dbd5888df3f0b19df141966d763e022
CodeWithSarmad <rajasarmadaz@gmail.com>
2026-02-13 16:37:37 +0500

    Layout done and about done

--- src/app/Home/Home.tsx
+++ src/app/Home/Home.tsx
@@ -1,5 +1,10 @@
+import About from '@/components/About'
+import Client from '@/components/Client'
+import FollowUs from '@/components/FollowUs'
+import Footer from '@/components/Footer'
 import Hero from '@/components/Hero'
 import Navbar from '@/components/Navbar'
+import Portfolio from '@/components/Portfolio'
 import React from 'react'
 
 const Home = () => {
@@ -7,6 +12,11 @@ const Home = () => {
     <div className='relative'>
         <Navbar />
         <Hero />
+        <About />
+        <Portfolio />
+        <Client />
+        <FollowUs />
+        <Footer />
     </div>
   )
 }
