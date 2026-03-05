5c7c0fa5b8f162a2c4a960734275b29be0db6a31
Muhammad Annas Khan <annasking601@gmail.com>
2026-03-05 13:18:27 +0500

    .

--- src/components/Logos.tsx
+++ src/components/Logos.tsx
@@ -9,9 +9,9 @@ import logo4 from "@/assets/logo4.png"
 
 const Logos = () => {
     return (
-        <section className="w-full bg-[#E6E6E6] py-6 px-4 md:px-8">
-            <div className="max-w-[1400px] mx-auto grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-8">
-                <div className="relative w-full h-32 md:h-40">
+        <section className="w-full bg-[#E6E6E6] overflow-hidden py-6 px-4 md:px-8">
+            <div className="max-w-[1400px] mx-auto items-center justify-center grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-10">
+                <div className="relative w-full h-30 md:h-40">
                     <Image
                         src={logo2}
                         alt="British Institute of Interior Design"
@@ -20,7 +20,7 @@ const Logos = () => {
                     />
                 </div>
 
-                <div className="relative w-full h-32 md:h-40">
+                <div className="relative w-full h-24 md:h-40">
                     <Image
                         src={logo3}
                         alt="Kingston Chamber of Commerce"
@@ -29,7 +29,7 @@ const Logos = () => {
                     />
                 </div>
 
-                <div className="relative w-full h-32 md:h-40 ml-25 md:ml-0">
+                <div className="relative w-full translate-x-25 h-30 md:h-40">
                     <Image
                         src={logo4}
                         alt="Kingston Green Business Community"
